7b259a3058058718e279806db2b1f4d1745ba07c
Lucassauro <lucassauro.lt@gmail.com>
2023-02-14 15:24:10 -0300

    Implemented test and refactored code to improve quality.

--- templates.py
+++ templates.py
@@ -3,30 +3,36 @@
 
 # the indentation is intentional.
 # The string should not use tabs to not broke markdown text when sent with request.
-DYNAMIC_TEMPLATE = "Beep Beep. I'm a BOT who doesn't like Paywalls. 🤖 \n \
-\n \
-I do provide alternative links so you can read your news or articles without those annoying things. \n\
-\n \
-Here are your links: \
-\n \
-\n \
+DYNAMIC_TEMPLATE = "Beep Beep. I'm a BOT who doesn't like Paywalls. 🤖\n\
+\n\
+I do provide alternative links so you can read your news or articles without those annoying things.\n\
+\n\
+Here are your links:\
+\n\
+\n\
 "
 # the indentation is intentional.
 # The string should not use tabs to not broke markdown text when sent with request.
-FIXED_TEMPLATE = "Beep Beep. I'm a BOT who doesn't like Paywalls. 🤖 \n \
-\n \
-I do provide links to news or articles without those annoying things. \n\
-\n \
-Here are your links in sequence: \
-\n \
-\n \
-- {} \
-\n \
-\n \
-- {} \
-\n \
-\n \
+FIXED_TEMPLATE = "Beep Beep. I'm a BOT who doesn't like Paywalls. 🤖\n\
+\n\
+I do provide alternative links so you can read your news or articles without those annoying things.\n\
+\n\
+Here are your links:\
+\n\
+\n\
+- {}\
+\n\
+\n\
+- {}\
+\n\
+\n\
 Beep beep. Bye."
+
+# pylint: disable-next=line-too-long
+MANY_LINKS_TEMPLATE = "I verified that there are more links, but I can only bypass three. Good luck with the rest.\n\
+\n"
+
+BYE_BYE_TEMPLATE = "Beep beep. Bye."
 
 FIRST_OPTION = "[First option](https://12ft.io/{})"
 SECOND_OPTION = "[Second option](https://www.removepaywall.com/{})"
@@ -34,4 +40,10 @@ SECOND_OPTION = "[Second option](https://www.removepaywall.com/{})"
 MULTI_FIRST_OPTION = "https://12ft.io/{}"
 MULTI_SECOND_OPTION = "https://www.removepaywall.com/{}"
 
-REGEX_PATTERN = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+# works at Regexr, but re.findall() return tuples when regex has capturing groups:
+# REGEX_PATTERN = r"\[(.+)?\]\(((?:\/|https?:\/\/).+)\)"
+
+#  without capturing groups re.findall() returns a list of correspondent strings:
+REGEX_PATTERN_MD_LINK = r"\[(?:.+)?\]\((?:\/|https?:\/\/.+)\)"
+
+REGEX_PATTERN_URL = r"\(((?:\/|https?:\/\/).+)\)"
--- utils.py
+++ utils.py
@@ -1,31 +1,38 @@
 
 """Module utils. Provides utilitary functions."""
-
 import re
 import templates
 
 
-def regex_find(text):
-    """regex_find function checks a string for regex pattern
-    and returns a list with all correspondences.
+def regex_find_markdown_link(text):
+    """regex_find_markdown_link function checks a string for regex pattern
+    and returns a list with all correspondences in markdown format.
     """
-    reg = re.findall(templates.REGEX_PATTERN, text)
-    for index, elem in enumerate(reg):
-        reg[index] = elem.rstrip(")")
-    return reg
+    return re.findall(templates.REGEX_PATTERN_MD_LINK, text)
+
+
+def regex_find_url(markdown_link_list):
+    """regex_find_url function checks a list of mardown formatted links
+    and returns a list with only URLs.
+    """
+    link_list = list()
+    for url in markdown_link_list:
+        matched_md = re.search(templates.REGEX_PATTERN_URL, url)
+        link = matched_md.group().lstrip("(").rstrip(")")
+        link_list.append(link)
+    return link_list
 
 
 def build_reply_text(link_list):
     """build_reply_text function builds a string to be used as a reply text."""
     reply_text = templates.DYNAMIC_TEMPLATE
-
     for index, url in enumerate(link_list):
         if index >= 3:
+            reply_text += templates.MANY_LINKS_TEMPLATE
             break
         # pylint: disable-next=line-too-long
-        reply_text += f"{index + 1}º Link: [First Option]({templates.MULTI_FIRST_OPTION.format(url)}); [Second Option]({templates.MULTI_SECOND_OPTION.format(url)})  \n \
-\n \
+        reply_text += f"{index + 1}º Link: [First Option]({templates.MULTI_FIRST_OPTION.format(url)}); [Second Option]({templates.MULTI_SECOND_OPTION.format(url)})\n\
+\n\
 "
-    reply_text += "Beep beep. Bye."
-
+    reply_text += templates.BYE_BYE_TEMPLATE
     return reply_text
